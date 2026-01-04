6f65ec9d32ecd0701b7a00d27b7a225907744922
Daniel Georgiev <georgiev.daniel@outlook.com>
2026-01-04 15:18:13 +0200

    (feat) add widget model and update gitignore

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/georgiev098/virtual-credit-card-terminal-go/internal/models"
+)
 
 func (app *Application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	if err := app.renderTemplate(w, r, "terminal", nil, "stripe-js"); err != nil {
@@ -40,7 +44,21 @@ func (app *Application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 }
 
 func (app *Application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "buy-once", nil, "stripe-js"); err != nil {
+
+	widget := models.Widget{
+		ID:             1,
+		Name:           "Custom widget",
+		Description:    "A very nice widget",
+		InventoryLevel: 10,
+		Price:          1000,
+	}
+
+	data := make(map[string]any)
+	data["widget"] = widget
+
+	if err := app.renderTemplate(w, r, "buy-once", &templateData{
+		Data: data,
+	}, "stripe-js"); err != nil {
 		app.ErrorLog.Println(err)
 	}
 }
