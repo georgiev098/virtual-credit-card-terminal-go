f15562869f6804f7c66b1d188c5eb1adc24d4da2
Daniel Georgiev <georgiev.daniel@outlook.com>
2026-01-06 17:24:32 +0200

    (feat) implement database models and add image to widget in db migration

--- internal/models/models.go
+++ internal/models/models.go
@@ -32,6 +32,53 @@ type Widget struct {
 	UpdateddAt     time.Time `json:"-"`
 }
 
+type Order struct {
+	ID            int       `json:"id"`
+	WidgetId      int       `json:"widget_id"`
+	TransactionID int       `json:"transaction_id"`
+	StatusID      int       `json:"status_id"`
+	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
+	CreatedAt     time.Time `json:"-"`
+	UpdateddAt    time.Time `json:"-"`
+}
+
+type Status struct {
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	CreatedAt  time.Time `json:"-"`
+	UpdateddAt time.Time `json:"-"`
+}
+
+type TransationStatus struct {
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	CreatedAt  time.Time `json:"-"`
+	UpdateddAt time.Time `json:"-"`
+}
+
+type Transation struct {
+	ID         int       `json:"id"`
+	FirstName  string    `json:"first_name"`
+	LastName   string    `json:"last_name"`
+	Email      string    `json:"email"`
+	Password   string    `json:"password"`
+	CreatedAt  time.Time `json:"-"`
+	UpdateddAt time.Time `json:"-"`
+}
+
+type User struct {
+	ID                  int       `json:"id"`
+	Amount              int       `json:"amount"`
+	TransactionStatusId int       `json:"transaction_status_id"`
+	Currency            string    `json:"currency"`
+	LastFour            string    `json:"last_four"`
+	BankReturnCode      string    `json:"bank_return_code"`
+	Name                string    `json:"name"`
+	CreatedAt           time.Time `json:"-"`
+	UpdateddAt          time.Time `json:"-"`
+}
+
 func (m *DBModel) GetWidget(id int) (Widget, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
