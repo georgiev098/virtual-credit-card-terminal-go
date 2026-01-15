9d5daa2f0b5cbaec15e694ba71a7c00c0c35f1f6
Daniel Georgiev <georgiev.daniel@outlook.com>
2026-01-15 21:27:55 +0200

    (feat) implement saving token to storage on FE

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -44,11 +44,18 @@ func (m *DBModel) InsertToken(token *Token, u User) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `INSERT INTO tokens (user_id, name, email, token_hash, created_at, updated_at) 
+	// delete existing tokens
+	stmt := `DELETE FROM tokens WHERE user_id = ?`
+	_, err := m.DB.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
+
+	stmt = `INSERT INTO tokens (user_id, name, email, token_hash, created_at, updated_at) 
 	values (?, ?, ?, ?, ?, ?)
 	`
 
-	_, err := m.DB.ExecContext(ctx, stmt, u.ID, u.LastName, u.Email, token.Hash, time.Now(), time.Now())
+	_, err = m.DB.ExecContext(ctx, stmt, u.ID, u.LastName, u.Email, token.Hash, time.Now(), time.Now())
 	if err != nil {
 		return err
 	}
